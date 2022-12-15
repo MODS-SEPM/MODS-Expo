56815cafb26f640cb8f59b6a58cf28d6756479a1
shreyavmatt <21IMC10059@fh-krems.ac.at>
2022-12-15 09:14:08 +0100

    Merge branch 'main' of https://github.com/MODS-SEPM/MODS-Expo
    # Conflicts:

--- UI/src/screens/AudioScreen.js
+++ UI/src/screens/AudioScreen.js
@@ -1,32 +1,63 @@
 import Background from "../components/Background";
 import React from "react";
-import {StyleSheet} from "react-native";
+// import {StyleSheet} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 // import SoundPlayer from 'react-native-sound-player'
 // import { TrackPlayer } from "react-native-track-player";
 import Header from '../components/Header'
+import {Button} from "react-native-paper";
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default function AudioScreen() {
   return (
-      <Background>
+        <Background>
 
-          <Header>Audio Player</Header>
+
+            <Header>Audio Player</Header>
+            <View style={{ flexDirection:"row", justifyContent: 'center' }}>
+                <View style={styles.buttonStyle}>
+                    <Button
+                        icon="skip-previous"
+                        mode="contained"
+                        onPress={() => console.log('Pressed previous')}
+                    />
+                </View>
+                <View style={styles.buttonStyle}>
+                    <Button
+                        icon="play"
+                        mode="contained"
+                        // onPress={pauseButton()}
+                        onPress={() => console.log('Pressed play')}
+                    />
+                </View>
+                <View style={styles.buttonStyle}>
+                    <Button
+                        icon="skip-next"
+                        mode="contained"
+                        onPress={() => console.log('Pressed next')}
+                    />
+                </View>
+            </View>
 
          {/* TrackPlayer.load({{
           title: "Awesome song",
-          artist: "Mr. Awesome",
-          album: "Awesome songs only",
-          cover: "https://source.unsplash.com/random",
+@ -19,23 +64,28 @@ export default function AudioScreen() {
           path: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
       }}).then(() => {
     console.log("audio loaded")};
   });
 };
+    });
+    };
 
 play = () => {
+    play = () => {
           TrackPlayer.play()};
       };
 
 pause = () => {
+    pause = () => {
           TrackPlayer.pause()};
       };*/}
 
@@ -35,7 +66,14 @@ pause = () => {
 
 const styles = StyleSheet.create({
   map: {
+    map: {
     height: '50%',
     width: '50%',
   },
+    },
+    buttonStyle: {
+    marginHorizontal: 25,
+    marginTop: 575,
+    justifyContent: 'center',
+  }
 });
